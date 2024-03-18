2a879c25fc05230b598f7574332d68836d71caec
Muhammad Sinar Agusta <sinaragusta112@gmail.com>
2024-03-18 09:37:38 +0700

    Tugas 4

--- Tugas 4/organisasi_x.py
+++ Tugas 4/organisasi_x.py
@@ -1,5 +1,4 @@
 anggota_tubuh_cacat = input("Apakah anda memmiliki anggota tubuh cacat? ")
-
 jenis_kelamin = input("Masukkan jenis kelamin :")
 berat_badan = int(input("Masukkan berat badan :"))
 tinggi_badan = int(input("Masukkan tinggi badan :"))
@@ -11,24 +10,13 @@ skill = input("Masukkan Skill :")
 
 if anggota_tubuh_cacat == "ya":
   print(f"Anda tidak memenuhi kriteria karena memilik anggota tubuh cacat")
+elif jenis_kelamin == "perempuan" and berat_badan >= 45 and berat_badan <= 50 and tinggi_badan >= 165 and usia <= 20:
+    print(f"Anda memenuhi kriteria 1")
+elif jenis_kelamin == "laki laki" and berat_badan <= 70 and tinggi_badan >= 170 and usia <= 30:
+    print(f"Anda memenuhi kriteria 2")
+elif jenis_kelamin == "laki laki" or jenis_kelamin == "perempuan" and nilai_akademis >= 90 and usia <= 30:
+    print(f"Anda memnuhi kriteria 3")
+elif jenis_kelamin == "laki laki" or jenis_kelamin == "perempuan" and skill == "menembak" or skill == "memanah" or skill == "berkuda":
+    print(f"Anda memenuhi kriteria 4")
 else:
-  if jenis_kelamin == "perempuan" and berat_badan >= 45:
-    if berat_badan <= 50 and tinggi_badan >= 165 and usia <= 20:
-      print(f"Anda memenuhi kriteria 1")
-    else:
-      print(f"Anda tidak memenuhi kriteria 1")
-  elif jenis_kelamin == "laki laki" and berat_badan <= 70:
-    if tinggi_badan >= 170 and usia <= 30:
-      print(f"Anda memenuhi kriteria 2")
-    else:
-      print(f"Anda tidak memenuhi kriteria 2")
-  if jenis_kelamin == "laki laki" or jenis_kelamin == "perempuan":
-    if nilai_akademis >= 90 and usia <= 30:
-      print(f"Anda memnuhi kriteria 3")
-    else:
-      print(f"Anda tidak memnuhi kriteria 3")
-  if jenis_kelamin == "laki laki" or jenis_kelamin == "perempuan":
-    if skill == "menembak" or skill == "memanah" or skill == "berkuda":
-      print(f"Anda memenuhi kriteria 4")
-    else:
-      print(f"anda tidak memenuhi kriteria 4")
+  print(f"Anda tidak lolos karena tidak memenuhi kriteria")
