4941c62586f90f00d1384df61e59511e0490081a
Muhammad Sinar Agusta <sinaragusta112@gmail.com>
2024-05-04 12:46:26 +0700

    Python Learning

--- Tugas 6/UTS/essay_1.py
+++
@@ -1,5 +0,0 @@
-for i in range(7,0,-1):
-  for j in range(7):
-      
-    print(i, end=" ")
-  print()
---
+++ Tugas/UTS/essay_1.py
@@ -0,0 +1,5 @@
+for i in range(7,0,-1):
+  for j in range(7):
+      
+    print(i, end=" ")
+  print()
